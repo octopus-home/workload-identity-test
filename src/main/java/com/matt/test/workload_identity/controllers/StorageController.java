890bebfe956f9b6498bc998c897670cd8278fbd4
octopus <806226383@qq.com>
2024-07-26 17:33:06 +0800

    can upload/download to storage account

---
+++ src/main/java/com/matt/test/workload_identity/controllers/StorageController.java
@@ -0,0 +1,72 @@
+package com.matt.test.workload_identity.controllers;
+
+import com.azure.core.util.BinaryData;
+import com.azure.core.util.Context;
+import com.azure.spring.cloud.core.resource.AzureStorageBlobProtocolResolver;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.options.BlockBlobSimpleUploadOptions;
+import com.azure.storage.blob.specialized.BlockBlobClient;
+import com.matt.test.workload_identity.service.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.WritableResource;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.time.Duration;
+import java.util.Map;
+
+@RestController
+public class StorageController {
+    static final String BLOB_RESOURCE_PATTERN = "azure-blob://%s/%s";
+    static final String containerName = "ftpcontainer";
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+    @Autowired
+    private AzureStorageBlobProtocolResolver azureStorageBlobProtocolResolver;
+    @Autowired
+    private BlobServiceClient blobServiceClient;
+    @Autowired
+    private StorageService service;
+
+    @GetMapping("/hello/{msg}")
+    public String hello(@PathVariable("msg") String msg) {
+        BlobContainerClient blobContainerClient = blobServiceClient.getBlobContainerClient(containerName);
+        BlockBlobClient blockBlobClient = blobContainerClient.getBlobClient(msg).getBlockBlobClient();
+
+        String content = "hello world! " + msg;
+
+        try (ByteArrayInputStream dataStream = new ByteArrayInputStream(content.getBytes())) {
+            BlockBlobSimpleUploadOptions options = new BlockBlobSimpleUploadOptions(dataStream,
+                    content.length()).setMetadata(Map.of("application_id", "123456")).setTags(Map.of("user_id", "hk_user"));
+            blockBlobClient.uploadWithResponse(options, Duration.ofSeconds(30L), Context.NONE);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return "hello " + msg;
+    }
+
+    @GetMapping("/{fileName}")
+    public String readResource(@PathVariable("fileName") String fileName) throws IOException {
+        Resource resource = resourceLoader.getResource(String.format(BLOB_RESOURCE_PATTERN, containerName, fileName));
+        return StreamUtils.copyToString(resource.getInputStream(), Charset.defaultCharset());
+    }
+
+    @PostMapping("/{fileName}")
+    public String writeResource(@PathVariable("fileName") String fileName, @RequestBody String data) throws IOException {
+
+        Resource resource = resourceLoader.getResource(String.format(BLOB_RESOURCE_PATTERN, this.containerName, fileName));
+        try (OutputStream os = ((WritableResource) resource).getOutputStream()) {
+            os.write(data.getBytes());
+        }
+        return "blob was updated";
+    }
+}
--- src/main/java/com/matt/test/workload_identity/repos/WiDemoRepo.java
+++ src/main/java/com/matt/test/workload_identity/repos/WiDemoRepo.java
@@ -1,8 +1,8 @@
-package com.matt.test.workload_identity.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.matt.test.workload_identity.models.WiDemo;
-
-public interface WiDemoRepo extends JpaRepository<WiDemo, Integer> {
-    
-}
+//package com.matt.test.workload_identity.repos;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import com.matt.test.workload_identity.models.WiDemo;
+//
+//public interface WiDemoRepo extends JpaRepository<WiDemo, Integer> {
+//
+//}
