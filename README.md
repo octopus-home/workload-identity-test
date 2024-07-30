89e4c5e4753194d9c6b329d27e30f13cd2a37547
octopus <806226383@qq.com>
2024-07-30 20:46:52 +0800

    workload identity rest api integration

--- README.md
+++ README.md
@@ -10,12 +10,12 @@ Before run
 used command as below
 ---
 ```
-az aks create --resource-group=matt_learn --name=m01akscluster --attach-acr m01registry --dns-name-prefix=m01aksclusterkubernetes --generate-ssh-keys
+az aks create --resource-group=matt_learn --name=m01akscluster --attach-acr m02registry --dns-name-prefix=m01aksclusterkubernetes --generate-ssh-keys
 az aks show -g matt_learn -n m01akscluster
 az aks get-credentials --resource-group=matt_learn --name=m01akscluster
 az aks show --name m01akscluster --resource-group matt_learn --query "oidcIssuerProfile.issuerUrl" --output tsv
 
-kubectl run workload-identity-docker --image=m01registry.azurecr.io/workload-identity:v0
+kubectl run workload-identity-docker --image=m02registry.azurecr.io/workload-identity:v0
 
 kubectl expose pod workload-identity-docker --type=LoadBalancer --port=80 --target-port=8080
 
@@ -24,16 +24,16 @@ networkWatchers
 az group deployment list --resource-group NetworkWatcherRG --query "[?properties.targetResourceGroup=='NetworkWatcherRG'].{Name:name, Timestamp:properties.timestamp}"
 
 az acr login --name m02registry --resource-group matt_learn
-az acr repository delete --name m01registry --image workload-identity
+az acr repository delete --name m02registry --image workload-identity
 
-docker build -t m01registry.azurecr.io/workload-identity .
-docker push m01registry.azurecr.io/workload-identity
+docker build -t m02registry.azurecr.io/workload-identity-rest .
+docker push m02registry.azurecr.io/workload-identity-rest
 
 
-kubectl run workload-identity-credential --image=m01registry.azurecr.io/workload-identity-credential
+kubectl run workload-identity-credential --image=m02registry.azurecr.io/workload-identity-credential
 kubectl expose pod workload-identity-credential --type=LoadBalancer --port=80 --target-port=8080
 
-kubectl run workload-identity-spn --image=m01registry.azurecr.io/workload-identity-spn --env="dbspn=726e2f44-b628-44c8-b726-720c29886427"
+kubectl run workload-identity-spn --image=m02registry.azurecr.io/workload-identity-spn --env="dbspn=726e2f44-b628-44c8-b726-720c29886427"
 kubectl patch pod workload-identity-spn -p '{"metadata":{"labels":{"azure.workload.identity/use":"true"}}}'
 
 
@@ -55,18 +55,15 @@ cat <<EOF | kubectl apply -f -
 apiVersion: v1
 kind: Pod
 metadata:
-  name: workload-identity
+  name: workload-identity-rest
   namespace: default
   labels:
     azure.workload.identity/use: "true"
 spec:
   serviceAccountName: workload-identity-sa
   containers:
-    - image: m01registry.azurecr.io/workload-identity
-      name: workload-identity
-      env:
-      - name: dbspn
-        value: 726e2f44-b628-44c8-b726-720c29886427
+    - image: m02registry.azurecr.io/workload-identity-rest
+      name: workload-identity-rest
 EOF
 
 kubectl expose pod workload-identity-spn --type=LoadBalancer --port=80 --target-port=8080
--- src/main/java/com/matt/test/workload_identity/controllers/StorageController.java
+++ src/main/java/com/matt/test/workload_identity/controllers/StorageController.java
@@ -1,89 +1,200 @@
 package com.matt.test.workload_identity.controllers;
 
-import com.azure.core.util.Context;
-import com.azure.identity.ClientSecretCredential;
-import com.azure.identity.ClientSecretCredentialBuilder;
-import com.azure.identity.ManagedIdentityCredential;
-import com.azure.identity.ManagedIdentityCredentialBuilder;
-import com.azure.storage.blob.BlobClient;
-import com.azure.storage.blob.BlobContainerClient;
-import com.azure.storage.blob.BlobContainerClientBuilder;
-import com.azure.storage.blob.options.BlockBlobSimpleUploadOptions;
-import com.azure.storage.blob.specialized.BlockBlobClient;
-import org.springframework.util.StreamUtils;
-import org.springframework.web.bind.annotation.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.time.Duration;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class StorageController {
-    static final String containerName = "container1";
+    static final String CONTAINER_NAME = "container1";
     private static final String CLIENT_ID = "580c9d1d-a8d1-4ffa-8d75-610807bc6240";
     private static final String CLIENT_SECRET = "xxx";
     private static final String TENANT_ID = "186a3027-ecbc-40e9-8bd6-2ccdcbc15e61";
     private static final String STORAGE_ENDPOINT = "https://m03storageaccount.blob.core.windows.net";
 
-    private static BlobContainerClient blobContainerClientBuild() {
-        //clientSecretCrdential build
-//        ClientSecretCredential identityCredential = new ClientSecretCredentialBuilder()
-//                .clientId(CLIENT_ID)
-//                .clientSecret(CLIENT_SECRET)
-//                .tenantId(TENANT_ID)
-//                .build();
+    private ObjectMapper mapper = new ObjectMapper();
 
-        ManagedIdentityCredential identityCredential = new ManagedIdentityCredentialBuilder()
-                .clientId(CLIENT_ID)
-                .build();
-
-
-        BlobContainerClient blobContainerClient = new BlobContainerClientBuilder()
-                .endpoint(STORAGE_ENDPOINT)
-                .credential(identityCredential)
-                .containerName(containerName)
-                .buildClient();
-        return blobContainerClient;
-    }
 
     @GetMapping("/hello/{msg}")
     public String hello(@PathVariable("msg") String msg) {
         return "hello " + msg;
     }
 
-    @GetMapping("/{fileName}")
-    public String readResource(@PathVariable("fileName") String fileName) throws IOException {
-        BlobContainerClient blobContainerClient = blobContainerClientBuild();
-        BlobClient blobClient = blobContainerClient.getBlobClient(fileName);
-        String fileContent = null;
-        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
-            blobClient.downloadStream(outputStream);
-            fileContent = StreamUtils.copyToString(outputStream, Charset.defaultCharset());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return fileContent;
+    @GetMapping("/credential/{fileName}")
+    public String readResource(@PathVariable("fileName") String fileName) throws IOException, InterruptedException, NoSuchAlgorithmException, KeyManagementException {
+        String access_token = getAccessToken();
+
+        SSLContext sslContext = ignoreSSLContext();
+        HttpClient blobClient = HttpClient.newBuilder().sslContext(sslContext).build();
+
+        HttpRequest getRequest = HttpRequest.newBuilder().uri(URI.create(STORAGE_ENDPOINT + "/" + CONTAINER_NAME + "/" + fileName))
+                .header("x-ms-version", "2024-08-04")
+                .header("Authorization", "Bearer " + access_token)
+                .GET()
+                .build();
+        String blob_content = blobClient.send(getRequest, HttpResponse.BodyHandlers.ofString()).body();
+
+        return blob_content;
     }
 
-    @PostMapping("/{fileName}")
-    public String writeResource(@PathVariable("fileName") String fileName, @RequestBody String data) throws IOException {
-        BlobContainerClient blobContainerClient = blobContainerClientBuild();
+    private String getAccessToken() throws IOException, InterruptedException {
+        HttpClient authClient = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://login.microsoftonline.com/" + TENANT_ID
+                        + "/oauth2/v2.0/token")).header("Content-Type", "application/x-www-form-urlencoded")
+                .POST(HttpRequest.BodyPublishers.ofString(
+                        "client_id=" + CLIENT_ID +
+                                "&client_secret=" + CLIENT_SECRET +
+                                "&grant_type=client_credentials" +
+                                "&scope=https://storage.azure.com/.default"
+                )).build();
+        String tokenResult = authClient.send(request, HttpResponse.BodyHandlers.ofString()).body();
+        HashMap mapResult = mapper.readValue(tokenResult, HashMap.class);
+        String access_token = (String) mapResult.get("access_token");
+        return access_token;
+    }
 
-        BlockBlobClient blockBlobClient = blobContainerClient.getBlobClient(fileName).getBlockBlobClient();
+    private static SSLContext ignoreSSLContext() throws NoSuchAlgorithmException, KeyManagementException {
+        TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return new X509Certificate[0];
+                    }
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                    }
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                    }
+                }
+        };
+
+        SSLContext sslContext = SSLContext.getInstance("SSL");
+        sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+        return sslContext;
+    }
+
+    @PostMapping("/credential/{fileName}")
+    public Integer writeResource(@PathVariable("fileName") String fileName, @RequestBody String data) throws IOException, InterruptedException, NoSuchAlgorithmException, KeyManagementException {
 
         String content = "hello world! " + data;
 
-        try (ByteArrayInputStream dataStream = new ByteArrayInputStream(content.getBytes())) {
-            BlockBlobSimpleUploadOptions options = new BlockBlobSimpleUploadOptions(dataStream,
-                    content.length()).setMetadata(Map.of("application_id", "123456")).setTags(Map.of("user_id", "hk_user"));
-            blockBlobClient.uploadWithResponse(options, Duration.ofSeconds(30L), Context.NONE);
-        } catch (IOException ex) {
-            ex.printStackTrace();
+        String access_token = getAccessToken();
+
+        SSLContext sslContext = ignoreSSLContext();
+        HttpClient blobClient = HttpClient.newBuilder().sslContext(sslContext).build();
+
+        HttpRequest putRequest = HttpRequest.newBuilder().uri(URI.create(STORAGE_ENDPOINT + "/" + CONTAINER_NAME + "/" + fileName))
+                .header("x-ms-version", "2024-08-04")
+                .header("Authorization", "Bearer " + access_token)
+                .header("x-ms-blob-type", "BlockBlob" )
+                .header("x-ms-tags", "user_id=hk_user")
+                .header("x-ms-meta-doc_index", "application_id:123456")
+                .PUT(HttpRequest.BodyPublishers.ofString(content))
+                .build();
+
+        HttpResponse<String> response = blobClient.send(putRequest, HttpResponse.BodyHandlers.ofString());
+
+        return response.statusCode();
+    }
+
+
+    @GetMapping("/workload/{fileName}")
+    public String download(@PathVariable("fileName") String fileName) throws IOException, InterruptedException {
+
+        Map<String, String> env = System.getenv();
+        String clientAssertion;
+        try {
+            clientAssertion = new String(Files.readAllBytes(Paths.get(env.get("AZURE_FEDERATED_TOKEN_FILE"))),
+                    StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            System.out.printf("Error creating client application: %s", e.getMessage());
+            throw new RuntimeException(e);
+        }
+        HttpClient authClient = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://login.microsoftonline.com/" + TENANT_ID
+                        + "/oauth2/v2.0/token")).header("Content-Type", "application/x-www-form-urlencoded")
+                .POST(HttpRequest.BodyPublishers.ofString(
+                        "client_id=" + CLIENT_ID +
+                                "&client_assertion_type=" + "urn:ietf:params:oauth:client-assertion-type:jwt-bearer" +
+                                "&grant_type=client_credentials" +
+                                "&scope=https://storage.azure.com/.default"+
+                                "&client_assertion=" + clientAssertion
+                )).build();
+        String tokenResult = authClient.send(request, HttpResponse.BodyHandlers.ofString()).body();
+        HashMap mapResult = mapper.readValue(tokenResult, HashMap.class);
+        String access_token = (String) mapResult.get("access_token");
+
+        HttpClient blobClient = HttpClient.newHttpClient();
+        HttpRequest getRequest = HttpRequest.newBuilder().uri(URI.create(STORAGE_ENDPOINT + "/" + CONTAINER_NAME + "/" + fileName))
+                .header("x-ms-version", "2024-08-04")
+                .header("Authorization", "Bearer " + access_token)
+                .GET()
+                .build();
+        String blob_content = blobClient.send(getRequest, HttpResponse.BodyHandlers.ofString()).body();
+
+        return blob_content;
+    }
+
+    @PostMapping("/workload/{fileName}")
+    public Integer upload(@PathVariable("fileName") String fileName, @RequestBody String data) throws IOException, InterruptedException {
+
+        String content = "hello world! " + data;
+
+        Map<String, String> env = System.getenv();
+        String clientAssertion;
+        try {
+            clientAssertion = new String(Files.readAllBytes(Paths.get(env.get("AZURE_FEDERATED_TOKEN_FILE"))),
+                    StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            System.out.printf("Error creating client application: %s", e.getMessage());
+            throw new RuntimeException(e);
         }
 
-        return "blob was uploaded";
+        HttpClient authClient = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://login.microsoftonline.com/" + TENANT_ID
+                        + "/oauth2/v2.0/token")).header("Content-Type", "application/x-www-form-urlencoded")
+                .POST(HttpRequest.BodyPublishers.ofString(
+                        "client_id=" + CLIENT_ID +
+                                "&client_assertion_type=" + "urn:ietf:params:oauth:client-assertion-type:jwt-bearer" +
+                                "&grant_type=client_credentials" +
+                                "&scope=https://storage.azure.com/.default"+
+                                "&client_assertion=" + clientAssertion
+                )).build();
+        String tokenResult = authClient.send(request, HttpResponse.BodyHandlers.ofString()).body();
+        HashMap mapResult = mapper.readValue(tokenResult, HashMap.class);
+        String access_token = (String) mapResult.get("access_token");
+
+        HttpClient blobClient = HttpClient.newHttpClient();
+
+        HttpRequest putRequest = HttpRequest.newBuilder().uri(URI.create(STORAGE_ENDPOINT + "/" + CONTAINER_NAME + "/" + fileName))
+                .header("x-ms-version", "2024-08-04")
+                .header("Authorization", "Bearer " + access_token)
+                .header("x-ms-blob-type", "BlockBlob" )
+                .header("x-ms-tags", "user_id=hk_user")
+                .header("x-ms-meta-doc_index", "application_id:123456")
+                .PUT(HttpRequest.BodyPublishers.ofString(content))
+                .build();
+
+        HttpResponse<String> response = blobClient.send(putRequest, HttpResponse.BodyHandlers.ofString());
+
+        return response.statusCode();
     }
 }
